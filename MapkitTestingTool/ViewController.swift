e7876fc21852b73b81c250c7c4d726da73526140
Maiara Trevisol <maiara.trevisol@sap.com>
2021-09-13 08:39:49 -0300

    first commit

--- MapkitTestingTool/ViewController.swift
+++ MapkitTestingTool/ViewController.swift
@@ -6,12 +6,21 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
-
+    var coordinate2D = CLLocationCoordinate2DMake(40.8367321, 14.2468856)
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateMapRegion(rangeSpan: 100)
         // Do any additional setup after loading the view.
+    }
+    
+    func updateMapRegion(rangeSpan:CLLocationDistance){
+        let region = MKCoordinateRegion(center: coordinate2D, latitudinalMeters: 40.8367321, longitudinalMeters: 14.2468856)
+        mapView.region = region
     }
 
 
